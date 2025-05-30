e533c91958781d8531d5ec4d631cc27016ead3a5
jHoon99 <eldkf1597@naver.com>
2025-05-30 13:26:36 +0900

    API 호출 로직 구현

--- YAP/YAP/Source/Feature/FoodSearch/NutritionService.swift
+++ YAP/YAP/Source/Feature/FoodSearch/NutritionService.swift
@@ -29,7 +29,29 @@ class NutritionService: ObservableObject {
   // MARK: - API 호출 로직
   func fetchNutritionData(query: String) async throws -> [FoodItem] {
     
+    var components = URLComponents(string: URL)!
+    
+    components.queryItems = [
+      URLQueryItem(name: "servicekey", value: serviceKey),
+      URLQueryItem(name: "type", value: "json"),
+      URLQueryItem(name: "FOOD_NM_KR", value: query)
+    ]
+    
+    guard let url = components.url else {
+      throw URLError(.badURL)
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpResponse = response as? HTTPURLResponse,
+          (200..<299).contains(httpResponse.statusCode) else {
+      throw URLError(.badServerResponse)
+    }
+    
+    let decoder = JSONDecoder()
+    let foodData = try decoder.decode(NutritionResponse.self, from: data)
+    
+    return foodData.body.items
   }
-  
 }
 
